7bbc0cbbbeb9227abba5be3ea63e2e3cc149844b
Ilayda Kuran <ilaydakuran@hotmail.com>
2021-07-29 21:45:42 +0300

    new component added

---
+++ src/app/rssfeed/rssfeed.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rssfeed',
+  templateUrl: './rssfeed.component.html',
+  styleUrls: ['./rssfeed.component.css']
+})
+export class RssfeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
